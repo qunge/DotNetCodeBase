4ade2f53fdabfb087853020200b36bb16afab480
qunge <qunge1@outlook.com>
2020-12-09 23:41:58 +0800

    框架提交

---
+++ CodeBase/CodeBase/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeBase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void UploadFile()
+        {
+            var files = System.Web.HttpContext.Current.Request.Files;
+            if (files.Count > 0)
+            {
+                // 文件后缀名
+                string fileExt = Path.GetExtension(files[0].FileName);
+            }
+        }
+
+
+    }
+}
